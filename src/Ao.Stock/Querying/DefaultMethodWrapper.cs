e8ed0d6e590031f0e050ce3c180efe95b4775cd6
HCricle <15218450198@163.com>
2023-03-03 15:34:04 +0800

    Support BoolAsInteger

--- src/Ao.Stock/Querying/DefaultMethodWrapper.cs
+++ src/Ao.Stock/Querying/DefaultMethodWrapper.cs
@@ -5,19 +5,20 @@ namespace Ao.Stock.Querying
 {
     public class DefaultMethodWrapper : IMethodWrapper
     {
-        public static readonly DefaultMethodWrapper MySql = new DefaultMethodWrapper("`", "`", "'", "'");
-        public static readonly DefaultMethodWrapper SqlServer = new DefaultMethodWrapper("[", "]", "'", "'");
+        public static readonly DefaultMethodWrapper MySql = new DefaultMethodWrapper("`", "`", "'", "'",true);
+        public static readonly DefaultMethodWrapper SqlServer = new DefaultMethodWrapper("[", "]", "'", "'", true);
         public static readonly DefaultMethodWrapper MariaDB = MySql;
-        public static readonly DefaultMethodWrapper Sqlite = new DefaultMethodWrapper("`", "`", "'", "'");
-        public static readonly DefaultMethodWrapper Oracle = new DefaultMethodWrapper("\"", "\"", "'", "'");
-        public static readonly DefaultMethodWrapper PostgreSql = new DefaultMethodWrapper("\"", "\"", "'", "'");
+        public static readonly DefaultMethodWrapper Sqlite = new DefaultMethodWrapper("`", "`", "'", "'", true);
+        public static readonly DefaultMethodWrapper Oracle = new DefaultMethodWrapper("\"", "\"", "'", "'", false);
+        public static readonly DefaultMethodWrapper PostgreSql = new DefaultMethodWrapper("\"", "\"", "'", "'", false);
 
-        public DefaultMethodWrapper(string qutoStart, string qutoEnd, string valueStart, string valueEnd)
+        public DefaultMethodWrapper(string qutoStart, string qutoEnd, string valueStart, string valueEnd,bool boolAsInteger)
         {
             QutoStart = qutoStart;
             QutoEnd = qutoEnd;
             ValueStart = valueStart;
             ValueEnd = valueEnd;
+            BoolAsInteger = boolAsInteger;
         }
 
         public string QutoStart { get; }
@@ -27,6 +28,8 @@ namespace Ao.Stock.Querying
         public string ValueStart { get; }
 
         public string ValueEnd { get; }
+
+        public bool BoolAsInteger{ get; }
 
         public string Quto<T>(T input)
         {
@@ -62,10 +65,16 @@ namespace Ao.Stock.Querying
             }
             else if (input is bool b)
             {
-                return b ? "1" : "0";
+                if (BoolAsInteger)
+                {
+                    return b ? "1" : "0";
+                }
+                return b ? boolTrue : boolFalse;
             }
             return input.ToString();
         }
+        private static readonly string boolTrue = bool.TrueString.ToLower();
+        private static readonly string boolFalse = bool.FalseString.ToLower();
     }
 
 }
